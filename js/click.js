ddbcdcce28daa320a600e3754817510674d7480b
Nathan Xiao <XDN2369@hotmail.com>
2020-06-01 11:59:43 +1000

    Add timer and animations

--- js/click.js
+++ js/click.js
@@ -2,19 +2,30 @@ var clickArea = document.getElementById("click-area");
 var statClick = document.getElementById("stat-clicks");
 var statCPS = document.getElementById("stat-cps");
 var statDBC = document.getElementById("stat-dbc");
+var statTime = document.getElementById("stat-time");
 
 var clicks = 0;
-var cps = 0;
-var dbc = 0;
+var dClicks = 0;
 
-var lastClick = undefined;
+var lastClick = 0;
+var currentClick = 0;
 var timer = undefined;
-var time = 1;
+var time = 0;
+
+// Default 500ms for double clicks
+var dClickThreshold = 500;
 
 clickArea.addEventListener("click", (ev) => {
-    clicks++;
     if (timer == undefined) {
         timer = startTimer();
+    }
+    clicks++;
+    currentClick = Date.now();
+    if (currentClick - lastClick <= dClickThreshold) {
+        dClicks++;
+        lastClick = 0;
+    } else {
+        lastClick = currentClick;
     }
     updateStat();
 })
@@ -26,8 +37,9 @@ function tick() {
 
 function updateStat() {
     statClick.innerText = clicks;
-    statCPS.innerText = Math.round((clicks / time) * 100) / 100;;
-    statDBC.innerText = dbc;
+    statCPS.innerText = Math.round((clicks / (time == 0 ? 1 : time)) * 100) / 100;
+    statDBC.innerText = dClicks;
+    statTime.innerText = time + "s";
 }
 
 function startTimer() {
